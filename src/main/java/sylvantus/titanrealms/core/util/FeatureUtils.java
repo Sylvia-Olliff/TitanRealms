7ac519c86a94d37ad16a3018a0f50e7fe6aafe62
Sylvia Olliff <sylvia.olliff@genesys.com>
2021-02-26 08:14:16 -0500

    Added new entries to access transformers. Implemented FeatureUtils helper class and TitanRealmsTreeGenerator base class

--- src/main/java/sylvantus/titanrealms/core/util/FeatureUtils.java
+++ src/main/java/sylvantus/titanrealms/core/util/FeatureUtils.java
@@ -1,4 +1,385 @@
 package sylvantus.titanrealms.core.util;
 
+import net.minecraft.block.AirBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.gen.IWorldGenerationReader;
+import net.minecraft.world.gen.blockstateprovider.BlockStateProvider;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.feature.TreeFeature;
+import net.minecraft.world.gen.trunkplacer.AbstractTrunkPlacer;
+import sylvantus.titanrealms.core.world.feature.TitanRealmsTreeGenerator;
+import sylvantus.titanrealms.core.world.feature.config.TreeFeatureConfig;
+
+import java.util.Random;
+import java.util.Set;
+
 public class FeatureUtils {
+    public static void putLeafBlock(IWorldGenerationReader world, Random random, BlockPos pos, BlockStateProvider state, Set<BlockPos> leavesPos) {
+        if (/*leavesPos.contains(pos) ||*/ !TreeFeature.isReplaceableAt(world, pos))
+            return;
+
+        world.setBlockState(pos, state.getBlockState(random, pos), 3);
+        leavesPos.add(pos.toImmutable());
+    }
+
+    // TODO phase out other leaf circle algorithms
+    public static void makeLeafCircle(IWorldGenerationReader world, Random random, BlockPos centerPos, float radius, BlockStateProvider state, Set<BlockPos> leaves) {
+        // Normally, I'd use mutable pos here but there are multiple bits of logic down the line that force
+        // the pos to be immutable causing multiple same BlockPos instances to exist.
+        float radiusSquared = radius * radius;
+        putLeafBlock(world, random, centerPos, state, leaves);
+
+        // trace out a quadrant
+        for (int x = 0; x <= radius; x++) {
+            for (int z = 1; z <= radius; z++) {
+                // if we're inside the blob, fill it
+                if (x * x + z * z <= radiusSquared) {
+                    // do four at a time for easiness!
+                    putLeafBlock(world, random, centerPos.add(  x, 0,  z), state, leaves);
+                    putLeafBlock(world, random, centerPos.add( -x, 0, -z), state, leaves);
+                    putLeafBlock(world, random, centerPos.add( -z, 0,  x), state, leaves);
+                    putLeafBlock(world, random, centerPos.add(  z, 0, -x), state, leaves);
+                    // Confused how this circle pixel-filling algorithm works exactly? https://www.desmos.com/calculator/psqynhk21k
+                }
+            }
+        }
+    }
+
+    public static boolean hasAirAround(IWorldGenerationReader world, BlockPos pos) {
+        for (Direction e : directionsExceptDown) {
+            if (world.hasBlockState(pos, b -> b.getBlock() instanceof AirBlock)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean canRootGrowIn(IWorldReader world, BlockPos pos) {
+        BlockState blockState = world.getBlockState(pos);
+        Block blockID = blockState.getBlock();
+
+        if (blockState.isAir(world, pos)) {
+            // roots can grow through air if they are near a solid block
+            return FeatureUtils.isNearSolid(world, pos);
+        } else {
+            return (blockState.getBlockHardness(world, pos) >= 0)
+                    && (blockState.getMaterial() == Material.ORGANIC || blockState.getMaterial() == Material.EARTH || blockState.getMaterial() == Material.ROCK || blockState.getMaterial() == Material.WATER);
+        }
+    }
+
+    /**
+     * Draws a line from {x1, y1, z1} to {x2, y2, z2}
+     * This takes all variables for setting Branch
+     */
+    public static void drawBresenhamBranch(IWorldGenerationReader world, Random random, BlockPos from, BlockPos to, Set<BlockPos> state, MutableBoundingBox mbb, BaseTreeFeatureConfig config) {
+        for (BlockPos pixel : getBresenhamArrays(from, to)) {
+            AbstractTrunkPlacer.func_236911_a_(world, random, pixel, state, mbb, config);
+        }
+    }
+
+    /**
+     * Moves distance along the vector.
+     * <p>
+     * This goofy function takes a float between 0 and 1 for the angle, where 0 is 0 degrees, .5 is 180 degrees and 1 and 360 degrees.
+     * For the tilt, it takes a float between 0 and 1 where 0 is straight up, 0.5 is straight out and 1 is straight down.
+     */
+    public static BlockPos translate(BlockPos pos, double distance, double angle, double tilt) {
+        double rangle = angle * 2.0D * Math.PI;
+        double rtilt = tilt * Math.PI;
+
+        return pos.add(
+                Math.round(Math.sin(rangle) * Math.sin(rtilt) * distance),
+                Math.round(Math.cos(rtilt) * distance),
+                Math.round(Math.cos(rangle) * Math.sin(rtilt) * distance)
+        );
+    }
+
+    /**
+     * Draws a line from {x1, y1, z1} to {x2, y2, z2}
+     * This takes all variables for setting Branch
+     */
+    public static void drawBresenhamBranch(TitanRealmsTreeGenerator<? extends TreeFeatureConfig> generator, IWorld world, Random random, BlockPos from, BlockPos to, Set<BlockPos> state, MutableBoundingBox mbb, TreeFeatureConfig config) {
+        for (BlockPos pixel : getBresenhamArrays(from, to)) {
+            generator.setBranchBlockState(world, random, pixel, state, mbb, config);
+            //world.setBlockState(pixel, state);
+        }
+    }
+
+    /**
+     * Draws a line from {x1, y1, z1} to {x2, y2, z2}
+     * This just takes a BlockState, used to set Trunk
+     */
+    public static void drawBresenhamTree(IWorld world, BlockPos from, BlockPos to, BlockState state, Set<BlockPos> treepos) {
+        for (BlockPos pixel : getBresenhamArrays(from, to)) {
+            world.setBlockState(pixel, state, 3);
+            treepos.add(pixel.toImmutable());
+        }
+    }
+
+    /**
+     * Get an array of values that represent a line from point A to point B
+     */
+    public static BlockPos[] getBresenhamArrays(BlockPos src, BlockPos dest) {
+        return getBresenhamArrays(src.getX(), src.getY(), src.getZ(), dest.getX(), dest.getY(), dest.getZ());
+    }
+
+    /**
+     * Get an array of values that represent a line from point A to point B
+     * TODO: lazify this into an iterable?
+     */
+    public static BlockPos[] getBresenhamArrays(int x1, int y1, int z1, int x2, int y2, int z2) {
+        int i, dx, dy, dz, absDx, absDy, absDz, x_inc, y_inc, z_inc, err_1, err_2, doubleAbsDx, doubleAbsDy, doubleAbsDz;
+
+        BlockPos pixel = new BlockPos(x1, y1, z1);
+        BlockPos lineArray[];
+
+        dx = x2 - x1;
+        dy = y2 - y1;
+        dz = z2 - z1;
+        x_inc = (dx < 0) ? -1 : 1;
+        absDx = Math.abs(dx);
+        y_inc = (dy < 0) ? -1 : 1;
+        absDy = Math.abs(dy);
+        z_inc = (dz < 0) ? -1 : 1;
+        absDz = Math.abs(dz);
+        doubleAbsDx = absDx << 1;
+        doubleAbsDy = absDy << 1;
+        doubleAbsDz = absDz << 1;
+
+        if ((absDx >= absDy) && (absDx >= absDz)) {
+            err_1 = doubleAbsDy - absDx;
+            err_2 = doubleAbsDz - absDx;
+            lineArray = new BlockPos[absDx + 1];
+            for (i = 0; i < absDx; i++) {
+                lineArray[i] = pixel;
+                if (err_1 > 0) {
+                    pixel = pixel.up(y_inc);
+                    err_1 -= doubleAbsDx;
+                }
+                if (err_2 > 0) {
+                    pixel = pixel.south(z_inc);
+                    err_2 -= doubleAbsDx;
+                }
+                err_1 += doubleAbsDy;
+                err_2 += doubleAbsDz;
+                pixel = pixel.east(x_inc);
+            }
+        } else if ((absDy >= absDx) && (absDy >= absDz)) {
+            err_1 = doubleAbsDx - absDy;
+            err_2 = doubleAbsDz - absDy;
+            lineArray = new BlockPos[absDy + 1];
+            for (i = 0; i < absDy; i++) {
+                lineArray[i] = pixel;
+                if (err_1 > 0) {
+                    pixel = pixel.east(x_inc);
+                    err_1 -= doubleAbsDy;
+                }
+                if (err_2 > 0) {
+                    pixel = pixel.south(z_inc);
+                    err_2 -= doubleAbsDy;
+                }
+                err_1 += doubleAbsDx;
+                err_2 += doubleAbsDz;
+                pixel = pixel.up(y_inc);
+            }
+        } else {
+            err_1 = doubleAbsDy - absDz;
+            err_2 = doubleAbsDx - absDz;
+            lineArray = new BlockPos[absDz + 1];
+            for (i = 0; i < absDz; i++) {
+                lineArray[i] = pixel;
+                if (err_1 > 0) {
+                    pixel = pixel.up(y_inc);
+                    err_1 -= doubleAbsDz;
+                }
+                if (err_2 > 0) {
+                    pixel = pixel.east(x_inc);
+                    err_2 -= doubleAbsDz;
+                }
+                err_1 += doubleAbsDy;
+                err_2 += doubleAbsDx;
+                pixel = pixel.south(z_inc);
+            }
+        }
+        lineArray[lineArray.length - 1] = pixel;
+
+        return lineArray;
+    }
+
+    /**
+     * Put a leaf only in spots where leaves can go!
+     */
+    public static void putLeafBlock(IWorld world, BlockPos pos, BlockState state, Set<BlockPos> leavespos) {
+        BlockState whatsThere = world.getBlockState(pos);
+
+        if (whatsThere.canBeReplacedByLeaves(world, pos) && whatsThere.getBlock() != state.getBlock()) {
+            world.setBlockState(pos, state, 3);
+            leavespos.add(pos.toImmutable());
+        }
+    }
+
+    /**
+     * Gets either cobblestone or mossy cobblestone, randomly.  Used for ruins.
+     */
+    public static BlockState randStone(Random rand, int howMuch) {
+        return rand.nextInt(howMuch) >= 1 ? Blocks.COBBLESTONE.getDefaultState() : Blocks.MOSSY_COBBLESTONE.getDefaultState();
+    }
+
+    /**
+     * Checks an area to see if it consists of flat natural ground below and air above
+     */
+    // TODO: Parameter "rand" is unused. Is it worth keeping? -Androsa
+    public static boolean isAreaSuitable(IWorld world, Random rand, BlockPos pos, int width, int height, int depth) {
+        boolean flag = true;
+
+        // check if there's anything within the diameter
+        for (int cx = 0; cx < width; cx++) {
+            for (int cz = 0; cz < depth; cz++) {
+                BlockPos pos_ = pos.add(cx, 0, cz);
+                // check if the blocks even exist?
+                if (world.isBlockLoaded(pos_)) {
+                    // is there grass, dirt or stone below?
+                    Material m = world.getBlockState(pos_.down()).getMaterial();
+                    if (m != Material.EARTH && m != Material.ORGANIC && m != Material.ROCK) {
+                        flag = false;
+                    }
+
+                    for (int cy = 0; cy < height; cy++) {
+                        // blank space above?
+                        if (!world.isAirBlock(pos_.up(cy))) {
+                            flag = false;
+                        }
+                    }
+                } else {
+                    flag = false;
+                }
+            }
+        }
+
+        // Okie dokie
+        return flag;
+    }
+
+    /**
+     * Draw a giant blob of whatevs.
+     */
+    public static void drawBlob(IWorld world, BlockPos pos, int rad, BlockState state) {
+        // then trace out a quadrant
+        for (byte dx = 0; dx <= rad; dx++) {
+            for (byte dy = 0; dy <= rad; dy++) {
+                for (byte dz = 0; dz <= rad; dz++) {
+                    // determine how far we are from the center.
+                    int dist = 0;
+                    if (dx >= dy && dx >= dz) {
+                        dist = dx + (Math.max(dy, dz) >> 1) + (Math.min(dy, dz) >> 2);
+                    } else if (dy >= dx && dy >= dz) {
+                        dist = dy + (Math.max(dx, dz) >> 1) + (Math.min(dx, dz) >> 2);
+                    } else {
+                        dist = dz + (Math.max(dx, dy) >> 1) + (Math.min(dx, dy) >> 2);
+                    }
+
+
+                    // if we're inside the blob, fill it
+                    if (dist <= rad) {
+                        // do eight at a time for easiness!
+                        world.setBlockState(pos.add(+dx, +dy, +dz), state, 3);
+                        world.setBlockState(pos.add(+dx, +dy, -dz), state, 3);
+                        world.setBlockState(pos.add(-dx, +dy, +dz), state, 3);
+                        world.setBlockState(pos.add(-dx, +dy, -dz), state, 3);
+                        world.setBlockState(pos.add(+dx, -dy, +dz), state, 3);
+                        world.setBlockState(pos.add(+dx, -dy, -dz), state, 3);
+                        world.setBlockState(pos.add(-dx, -dy, +dz), state, 3);
+                        world.setBlockState(pos.add(-dx, -dy, -dz), state, 3);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Draw a giant blob of leaves.
+     */
+    public static void drawLeafBlob(IWorld world, BlockPos pos, int rad, BlockState state, Set<BlockPos> leaves) {
+        // then trace out a quadrant
+        for (byte dx = 0; dx <= rad; dx++) {
+            for (byte dy = 0; dy <= rad; dy++) {
+                for (byte dz = 0; dz <= rad; dz++) {
+                    // determine how far we are from the center.
+                    int dist;
+                    if (dx >= dy && dx >= dz) {
+                        dist = dx + (Math.max(dy, dz) >> 1) + (Math.min(dy, dz) >> 2);
+                    } else if (dy >= dx && dy >= dz) {
+                        dist = dy + (Math.max(dx, dz) >> 1) + (Math.min(dx, dz) >> 2);
+                    } else {
+                        dist = dz + (Math.max(dx, dy) >> 1) + (Math.min(dx, dy) >> 2);
+                    }
+
+                    // if we're inside the blob, fill it
+                    if (dist <= rad) {
+                        // do eight at a time for easiness!
+                        putLeafBlock(world, pos.add(+dx, +dy, +dz), state, leaves);
+                        putLeafBlock(world, pos.add(+dx, +dy, -dz), state, leaves);
+                        putLeafBlock(world, pos.add(-dx, +dy, +dz), state, leaves);
+                        putLeafBlock(world, pos.add(-dx, +dy, -dz), state, leaves);
+                        putLeafBlock(world, pos.add(+dx, -dy, +dz), state, leaves);
+                        putLeafBlock(world, pos.add(+dx, -dy, -dz), state, leaves);
+                        putLeafBlock(world, pos.add(-dx, -dy, +dz), state, leaves);
+                        putLeafBlock(world, pos.add(-dx, -dy, -dz), state, leaves);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Does the block have only air blocks adjacent
+     */
+    public static boolean surroundedByAir(IWorldReader world, BlockPos pos) {
+        for (Direction e : Direction.values()) {
+            if (!world.isAirBlock(pos.offset(e))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Does the block have at least 1 air block adjacent
+     */
+    private static final Direction[] directionsExceptDown = new Direction[]{Direction.UP, Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST};
+
+    public static boolean hasAirAround(IWorld world, BlockPos pos) {
+        for (Direction e : directionsExceptDown) {
+            if (world.isAirBlock(pos.offset(e))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isNearSolid(IWorldReader world, BlockPos pos) {
+        for (Direction e : Direction.values()) {
+            if (world.isBlockLoaded(pos.offset(e))
+                    && world.getBlockState(pos.offset(e)).getMaterial().isSolid()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void setBlockStateProvider(IWorld world, BlockStateProvider provider, Random rand, BlockPos pos) {
+        world.setBlockState(pos, provider.getBlockState(rand, pos), 3);
+    }
 }
--- src/main/java/sylvantus/titanrealms/core/world/feature/TitanRealmsTreeGenerator.java
+++ src/main/java/sylvantus/titanrealms/core/world/feature/TitanRealmsTreeGenerator.java
@@ -1,4 +1,191 @@
 package sylvantus.titanrealms.core.world.feature;
 
-public class TitanRealmsTreeGenerator {
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.mojang.serialization.Codec;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.shapes.BitSetVoxelShapePart;
+import net.minecraft.util.math.shapes.VoxelShapePart;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldWriter;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.TreeFeature;
+import net.minecraft.world.gen.feature.template.Template;
+import sylvantus.titanrealms.core.util.FeatureUtils;
+import sylvantus.titanrealms.core.world.feature.config.TreeFeatureConfig;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public abstract class TitanRealmsTreeGenerator<T extends TreeFeatureConfig> extends Feature<T> {
+
+    public TitanRealmsTreeGenerator(Codec<T> configIn) {
+        super(configIn);
+    }
+
+    protected boolean generate(IWorld world, Random random, BlockPos pos, Set<BlockPos> logpos, Set<BlockPos> leavespos, MutableBoundingBox mbb, T config) {
+        Set<BlockPos> branchSet = Sets.newHashSet();
+        Set<BlockPos> rootSet = Sets.newHashSet();
+        return generate(world, random, pos, logpos, leavespos, branchSet, rootSet, mbb, config);
+    }
+
+    //AbstractTreeFeature.place from 1.15, modified for us
+    @Override
+    public boolean generate(ISeedReader world, ChunkGenerator generator, Random random, BlockPos pos, T config) {
+        Set<BlockPos> logs = Sets.newHashSet();
+        Set<BlockPos> leaves = Sets.newHashSet();
+        MutableBoundingBox mutableboundingbox = MutableBoundingBox.getNewBoundingBox();
+        boolean flag = this.generate(world, random, pos, logs, leaves, mutableboundingbox, config);
+        if (mutableboundingbox.minX <= mutableboundingbox.maxX && flag && !logs.isEmpty()) {
+            VoxelShapePart voxelshapepart = this.getVoxelShapePart(world, mutableboundingbox, logs);
+            Template.func_222857_a(world, 3, voxelshapepart, mutableboundingbox.minX, mutableboundingbox.minY, mutableboundingbox.minZ);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //TreeFeature.func_227214_a_ copy, modified to remove decorations
+    private VoxelShapePart getVoxelShapePart(IWorld world, MutableBoundingBox mbb, Set<BlockPos> logPosSet) {
+        List<Set<BlockPos>> list = Lists.newArrayList();
+        VoxelShapePart voxelshapepart = new BitSetVoxelShapePart(mbb.getXSize(), mbb.getYSize(), mbb.getZSize());
+
+        for(int j = 0; j < 6; ++j) {
+            list.add(Sets.newHashSet());
+        }
+
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+
+        for(BlockPos logPos : Lists.newArrayList(logPosSet)) {
+            if (mbb.isVecInside(logPos)) {
+                voxelshapepart.setFilled(logPos.getX() - mbb.minX, logPos.getY() - mbb.minY, logPos.getZ() - mbb.minZ, true, true);
+            }
+
+            for(Direction direction : Direction.values()) {
+                mutable.setAndMove(logPos, direction);
+                if (!logPosSet.contains(mutable)) {
+                    BlockState blockstate = world.getBlockState(mutable);
+                    if (blockstate.hasProperty(BlockStateProperties.DISTANCE_1_7)) {
+                        list.get(0).add(mutable.toImmutable());
+                        TreeFeature.func_236408_b_(world, mutable, blockstate.with(BlockStateProperties.DISTANCE_1_7, 1));
+                        if (mbb.isVecInside(mutable)) {
+                            voxelshapepart.setFilled(mutable.getX() - mbb.minX, mutable.getY() - mbb.minY, mutable.getZ() - mbb.minZ, true, true);
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int l = 1; l < 6; ++l) {
+            Set<BlockPos> set = list.get(l - 1);
+            Set<BlockPos> set1 = list.get(l);
+
+            for(BlockPos blockpos2 : set) {
+                if (mbb.isVecInside(blockpos2)) {
+                    voxelshapepart.setFilled(blockpos2.getX() - mbb.minX, blockpos2.getY() - mbb.minY, blockpos2.getZ() - mbb.minZ, true, true);
+                }
+
+                for(Direction direction1 : Direction.values()) {
+                    mutable.setAndMove(blockpos2, direction1);
+                    if (!set.contains(mutable) && !set1.contains(mutable)) {
+                        BlockState blockstate1 = world.getBlockState(mutable);
+                        if (blockstate1.hasProperty(BlockStateProperties.DISTANCE_1_7)) {
+                            int k = blockstate1.get(BlockStateProperties.DISTANCE_1_7);
+                            if (k > l + 1) {
+                                BlockState blockstate2 = blockstate1.with(BlockStateProperties.DISTANCE_1_7, l + 1);
+                                TreeFeature.func_236408_b_(world, mutable, blockstate2);
+                                if (mbb.isVecInside(mutable)) {
+                                    voxelshapepart.setFilled(mutable.getX() - mbb.minX, mutable.getY() - mbb.minY, mutable.getZ() - mbb.minZ, true, true);
+                                }
+
+                                set1.add(mutable.toImmutable());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return voxelshapepart;
+    }
+
+    /**
+     * This works akin to the AbstractTreeFeature.generate, but put our branches and roots here
+     */
+    protected abstract boolean generate(IWorld world, Random random, BlockPos pos, Set<BlockPos> logpos, Set<BlockPos> leavespos, Set<BlockPos> branchpos, Set<BlockPos> rootpos, MutableBoundingBox mbb, T config);
+
+    //AbstractTrunkPlacer.func_236911_a_ copy - Use that one instead when extending that Abstract
+    protected boolean setLogBlockState(IWorld world, Random random, BlockPos pos, Set<BlockPos> logPos, MutableBoundingBox mbb, TreeFeatureConfig config) {
+        if (TreeFeature.isReplaceableAt(world, pos)) {
+            this.setBlockState(world, pos, config.trunkProvider.getBlockState(random, pos), mbb);
+            logPos.add(pos.toImmutable());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //We aren't actually using this, but it is here just in case
+    protected boolean setLeavesBlockState(IWorld world, Random random, BlockPos pos, Set<BlockPos> leavesPos, MutableBoundingBox mbb, TreeFeatureConfig config) {
+        if (TreeFeature.isReplaceableAt(world, pos)) {
+            this.setBlockState(world, pos, config.leavesProvider.getBlockState(random, pos), mbb);
+            leavesPos.add(pos.toImmutable());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean setBranchBlockState(IWorld world, Random random, BlockPos pos, Set<BlockPos> branchpos, MutableBoundingBox mbb, TreeFeatureConfig config) {
+        if (TreeFeature.isReplaceableAt(world, pos)) {
+            this.setBlockState(world, pos, config.branchProvider.getBlockState(random, pos), mbb);
+            branchpos.add(pos.toImmutable());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected boolean setRootsBlockState(IWorld world, Random random, BlockPos pos, Set<BlockPos> branchpos, MutableBoundingBox mbb, TreeFeatureConfig config) {
+        // XXX: This was originally an IWorld in AbstractTreeFeature.place, so it should be ok to cast it back.
+        // If you're here investigating after it blew up, then the above assumption is no longer true.
+        if (FeatureUtils.canRootGrowIn(world, pos)) {
+            this.setBlockState(world, pos, config.rootsProvider.getBlockState(random, pos), mbb);
+            branchpos.add(pos.toImmutable());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected final void setBlockState(IWorldWriter world, BlockPos pos, BlockState state, MutableBoundingBox mbb) {
+        world.setBlockState(pos, state, 19);
+        mbb.expandTo(new MutableBoundingBox(pos, pos));
+    }
+
+    /**
+     * Build a root, but don't let it stick out too far into thin air because that's weird
+     */
+    protected void buildRoot(IWorld world, Random rand, BlockPos pos, Set<BlockPos> setpos, double offset, int b, MutableBoundingBox mbb, T config) {
+        BlockPos dest = FeatureUtils.translate(pos.down(b + 2), 5, 0.3 * b + offset, 0.8);
+
+        // go through block by block and stop drawing when we head too far into open air
+        BlockPos[] lineArray = FeatureUtils.getBresenhamArrays(pos.down(), dest);
+        for (BlockPos coord : lineArray) {
+            this.setRootsBlockState(world, rand, coord, setpos, mbb, config);
+        }
+    }
+
+    private void setIfEmpty(IWorld world, BlockPos pos, BlockState state) {
+        if (world.isAirBlock(pos)) {
+            world.setBlockState(pos, state,3);
+        }
+    }
 }
